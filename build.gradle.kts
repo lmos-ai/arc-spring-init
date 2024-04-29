c6d7eaa4dc9f281f72dde4af388e04e56e3ae6c2
patrick.whelan <patrick.whelan@telekom.de>
2024-04-29 20:51:18 +0200

    feat: add Summarizer Agent

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ tasks.withType<KotlinCompile> {
 }
 
 dependencies {
-    val arcVersion = "0.20.0"
+    val arcVersion = "0.21.0"
     kotlinScriptDef("io.github.lmos-ai.arc:arc-scripting:$arcVersion")
     implementation("io.github.lmos-ai.arc:arc-scripting:$arcVersion")
     implementation("io.github.lmos-ai.arc:arc-azure-client:$arcVersion")
