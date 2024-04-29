c82981f02c8d66a8b7c6951dd28ba80a17ce6cc9
patrick.whelan <patrick.whelan@telekom.de>
2024-04-29 20:51:18 +0200

    feat: add Summarizer Agent

---
+++ agents/summarizer.agent.kts
@@ -0,0 +1,32 @@
+import io.github.lmos.arc.core.getOrThrow
+
+/*
+ * // SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+ * //
+ * // SPDX-License-Identifier: Apache-2.0
+ */
+
+agent {
+    name = "summarizer-agent"
+    description = "Agent that summarizes web pages."
+    systemPrompt = {
+        """
+       You are a helpful agent. 
+       You help customers by summarizing webpages. 
+       Keep your answer short and concise.
+     """
+    }
+    filterInput {
+        val url = extractUrl(inputMessage).firstOrNull()
+        if (url != null) {
+            debug("Loading url: $url")
+            val html = html(url).getOrThrow()
+            inputMessage = inputMessage.update("""
+                User question: ${inputMessage.content}
+                The webpage $url contains the following text:
+                $html
+             """
+            )
+        }
+    }
+}
