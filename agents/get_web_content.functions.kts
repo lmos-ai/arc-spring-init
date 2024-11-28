180b38b295bc4f6ec9986a398da846ffd8b2422f
patrick.whelan <patrick.whelan@telekom.de>
2024-11-28 12:30:48 +0100

    chore: add missing License

--- agents/get_web_content.functions.kts
+++ agents/get_web_content.functions.kts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Deutsche Telekom AG
+//
+// SPDX-License-Identifier: Apache-2.0
+
 function(
     name = "get_web_content",
     description = "Returns content from the web.",
